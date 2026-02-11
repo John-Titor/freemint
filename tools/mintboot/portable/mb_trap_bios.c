a3e3e790eaf2e3d761fe94d09203d34e20086361
none <user@example.com>
2026-02-11 08:22:41 -0800

    mintboot: add kbdvbase support

--- tools/mintboot/portable/mb_trap_bios.c
+++ tools/mintboot/portable/mb_trap_bios.c
@@ -26,6 +26,10 @@ static uint32_t mb_rom_le32(const uint8_t *ptr)
 	       ((uint32_t)ptr[2] << 16) | ((uint32_t)ptr[3] << 24);
 }
 
+static void mb_kbdvec_stub(void)
+{
+}
+
 /* BIOS (trap #13) stubs */
 long mb_rom_bconstat(uint16_t dev)
 {
@@ -256,8 +260,45 @@ long mb_rom_dosound(uint32_t ptr)
 
 long mb_rom_kbdvbase(void)
 {
-	mb_panic("Kbdvbase()");
-	return -1;
+	struct mb_kbdvecs {
+		uint32_t midivec;
+		uint32_t vkbderr;
+		uint32_t vmiderr;
+		uint32_t statvec;
+		uint32_t mousevec;
+		uint32_t clockvec;
+		uint32_t joyvec;
+		uint32_t midisys;
+		uint32_t ikbdsys;
+		uint8_t ikbdstate;
+		uint8_t kbdlength;
+		uint16_t pad;
+	} __attribute__((packed));
+	struct mb_kbdvbase {
+		uint32_t kbdvec;
+		struct mb_kbdvecs vecs;
+	} __attribute__((packed));
+	static struct mb_kbdvbase storage;
+	static int inited;
+
+	if (!inited) {
+		uint32_t stub = (uint32_t)(uintptr_t)mb_kbdvec_stub;
+		storage.kbdvec = stub;
+		storage.vecs.midivec = stub;
+		storage.vecs.vkbderr = stub;
+		storage.vecs.vmiderr = stub;
+		storage.vecs.statvec = stub;
+		storage.vecs.mousevec = stub;
+		storage.vecs.clockvec = stub;
+		storage.vecs.joyvec = stub;
+		storage.vecs.midisys = stub;
+		storage.vecs.ikbdsys = stub;
+		storage.vecs.ikbdstate = 0;
+		storage.vecs.kbdlength = 0;
+		inited = 1;
+	}
+
+	return (long)(uintptr_t)&storage.vecs;
 }
 
 long mb_rom_vsync(void)
